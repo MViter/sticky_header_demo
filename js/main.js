d23c71631f6ccd60436aec1a011b214221fdef1b
MViter <margarytaviter@gmail.com>
2018-01-19 14:48:16 +0200

    Editing styles and mediaqueries

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 $(window).scroll(function() {
       if ($(this).scrollTop() > 1){
       $('.header').addClass('sticky');
+      $('.logo').addClass('smallLogo');
       }
       else{
       $('.header').removeClass('sticky');
+      $('.logo').removeClass('smallLogo');
       }
 });
